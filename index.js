63955bd1a1dff7e7cc147ee6d010d41512e3bcac
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-03-17 09:35:20 +1300

    added mark and description vars, also wrote first if and switch statements

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 //Lab Three 
 //Jordan Huitema
+
+let mark = 85;
+const desA = "Pass with Distinction";
+const desB = "Pass with Merit";
+const desC = "Pass";
+const desD = "Fail grades";
+
+if (mark >= 85 || mark <= 100) {
+    switch(mark){
+        case (mark>=85):
+            console.log("Result = A+");
+            break
+        case (mark>=80):
+            console.log("Result = A");
+            break
+        case (mark>=75):
+            console.log("Result = A-");
+    }
+    console.log("Pass with Distinction")
+} 
